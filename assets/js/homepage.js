585c8d28754f4d1f2ee235ffcc93b9893f96341f
shiba inu <jefferson121996@gmail.com>
2022-04-09 11:54:38 -0400

    changes made

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,10 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,8 +12,31 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
+          displayRepos(data, user);
         console.log(data);
       });
-    });
+    }); 
   };
-  getUserRepos("facebook");
+
+  var formSubmitHandler = function(event){
+    event.preventDefault();
+    //get value from input element
+    var username = nameInputEl.value.trim();
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else{
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+}
+
+var displayRepos = function(repos, searchTerm){
+    console.log(repos);
+    console.log(searchTerm);
+    //clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+};
+
+  userFormEl.addEventListener("submit", formSubmitHandler);
